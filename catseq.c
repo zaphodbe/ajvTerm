8531ae0c7a18dd4dc4d8e5c07155e7a35496ac8e
vandys <vandys@6344e5a3-2e74-4e93-9979-b4b536feca08>
2009-04-12 02:11:22 +0000

    Rationalize the code.
    git-svn-id: file://localhost/home/vandys/src/svn/ajvTerm@74 6344e5a3-2e74-4e93-9979-b4b536feca08

--- catseq.c
+++ catseq.c
@@ -27,8 +27,20 @@
 #include <ctype.h>
 
 static FILE *fp, *trcf = NULL;
-static int done = 0;
 static unsigned int pos = 0;
+static struct termios told;
+
+/*
+ * done()
+ *	Report final position, restore TTY, and exit
+ */
+static void
+done(void)
+{
+    tcsetattr(1, TCSANOW, &told);
+    fprintf(stderr, "\nFinished at position %u\n", pos);
+    exit(0);
+}
 
 /*
  * nextc()
@@ -44,12 +56,11 @@ nextc(void)
 
     x = fgetc(fp);
     if (x == EOF) {
-	done = 1;
-    } else {
-	pos += 1;
-	if (trcf) {
-	    fputc(x, trcf);
-	}
+	done();
+    }
+    pos += 1;
+    if (trcf) {
+	fputc(x, trcf);
     }
     return(x);
 }
@@ -57,9 +68,9 @@ nextc(void)
 int
 main(int argc, char **argv)
 {
-    struct termios told, tnew;
     char c;
-    int x, nchar = 0, step, bychars = 0;
+    int x, bychars = 0, nout = 0, oldnout = 1;
+    struct termios tnew;
 
     /* Access file to display */
     if ((argc < 2) || (argc > 3)) {
@@ -84,72 +95,56 @@ main(int argc, char **argv)
     tcgetattr(1, &told);
     tnew = told;
     tnew.c_lflag &= ~(ICANON|ECHO);
+    tnew.c_oflag &= ~(ONLCR | OCRNL | OPOST);
     tnew.c_cc[VMIN] = 1;
     tnew.c_cc[VTIME] = 0;
     tcsetattr(1, TCSANOW, &tnew);
 
     /* Feed bytes */
-    do {
-	x = nextc();
-	if (x == EOF) {
-	    break;
-	}
-	c = x;
+    for (;;) {
+	/* Stop for keyboard input at required interval */
+	if (--nout <= 0) {
+	    (void)read(0, &c, 1);
 
-	/*
-	 * Things flow through until we hit an escape sequence,
-	 *  or we're in char-at-a-time mode.
-	 */
-	write(1, &c, 1);
-	if (!bychars && (c != 27)) {
-	    continue;
-	}
+	    /* Bail out? */
+	    if (c == 'q') {
+		done();
 
-	/* Copy until the end of this sequence */
-	for (;;) {
-	    x = nextc();
-	    if (x == EOF) {
-		break;
-	    }
-	    c = x;
-	    write(1, &c, 1);
+	    /* Toggle "by char" mode? */
+	    } else if (c == 'c') {
+		bychars = !bychars;
+		oldnout = nout = 1;
+
+	    /* Get a count of steps */
+	    } else if (isdigit(c)) {
+		oldnout = nout = c - '0';
 
 	    /*
-	     * We interact with the keyboard on each char if we're
-	     *  in "by chars" mode, otherwise at the end of the
-	     *  current escape sequence.
+	     * Any other key, step for the same number as
+	     *  last time.
 	     */
-	    if (bychars) {
-		step = 1;
-	    } else  {
-		step = (c != ';') && !isdigit(c) && (c != '[');
-	    }
-
-	    /* Time step interact with the keyboard? */
-	    if (step) {
-		if (nchar) {
-		    nchar -= 1;
-		} else {
-		    (void)read(0, &c, 1);
-
-		    /* Bail out? */
-		    if (c == 'q') {
-			done = 1;
-
-		    /* Toggle "by char" mode? */
-		    } else if (c == 'c') {
-			bychars = !bychars;
-
-		    /* Get a count of steps */
-		    } else if (isdigit(c)) {
-			nchar = c - '0';
-		    }
-		}
-		break;
+	    } else {
+		nout = oldnout;
 	    }
 	}
-    } while (!done);
-    tcsetattr(1, TCSANOW, &told);
-    fprintf(stderr, "\nFinished at position %u\n", pos);
-    return(0);
+
+	/* Output the next unit--a char or through an escape sequence */
+	if (bychars) {
+	    c = nextc();
+	    write(1, &c, 1);
+
+	} else {
+	    /* Walk into next escape sequence */
+	    do {
+		c = nextc();
+		write(1, &c, 1);
+	    } while (c != 27);
+
+	    /* Now walk through the sequence */
+	    do {
+		c = nextc();
+		write(1, &c, 1);
+	    } while ((c == ';') || isdigit(c) || (c == '['));
+	}
+    }
 }
